ae66899dbbb0716a6e219846b3316f93ce3081dc
chrisliu298 <cyliu298@proton.me>
2024-07-19 13:40:43 +0800

    Add rm code

---
+++ rm/run_reward_modeling.py
@@ -0,0 +1,144 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+import torch
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    HfArgumentParser,
+    TrainingArguments,
+)
+from utils import (
+    RewardDataCollatorWithPadding,
+    RewardTrainer,
+    build_dataset,
+    build_dataset_local,
+    compute_metrics,
+)
+
+
+@dataclass
+class ScriptArguments:
+    """
+    These arguments vary depending on how many GPUs you have, what their capacity and features are, and what size model you want to train.
+    """
+
+    per_device_train_batch_size: Optional[int] = field(default=1)
+    gradient_accumulation_steps: Optional[int] = field(default=64)
+    learning_rate: Optional[float] = field(default=1e-5)
+    weight_decay: Optional[float] = field(default=0.001)
+    model_name: Optional[str] = field(
+        default="Meta-Llama-3-8B-Instruct",
+        metadata={
+            "help": "The model that you want to train from the Hugging Face hub. E.g. gpt2, gpt2-xl, bert, etc."
+        },
+    )
+    bf16: Optional[bool] = field(
+        default=True,
+        metadata={
+            "help": "This essentially cuts the training time in half if you want to sacrifice a little precision and have a supported GPU."
+        },
+    )
+    num_train_epochs: Optional[int] = field(
+        default=1,
+        metadata={"help": "The number of training epochs for the reward model."},
+    )
+    train_set_path: Optional[str] = field(
+        default="preference_dataset_mixture2_and_safe_pku",
+        metadata={"help": "The dir of the subset of the training data to use"},
+    )
+    output_dir: Optional[str] = field(
+        default="./bt_models", metadata={"help": "The dir for output model"}
+    )
+    gradient_checkpointing: Optional[bool] = field(
+        default=True, metadata={"help": "Enables gradient checkpointing."}
+    )
+    optim: Optional[str] = field(
+        default="paged_adamw_32bit", metadata={"help": "The optimizer to use."}
+    )
+    lr_scheduler_type: Optional[str] = field(
+        default="cosine", metadata={"help": "The lr scheduler"}
+    )
+    max_length: Optional[int] = field(default=4096)
+    save_every_steps: Optional[int] = field(
+        default=999999, metadata={"help": "Save the model every x steps"}
+    )
+    load_data_from_local: Optional[bool] = field(
+        default=False, metadata={"help": "Load the data from local disk"}
+    )
+
+    def __post_init__(self):
+        if self.output_dir == "./bt_models":
+            self.output_dir = f"./bt_models/{self.model_name}_{self.train_set_path}"
+
+
+parser = HfArgumentParser(ScriptArguments)
+script_args = parser.parse_args_into_dataclasses()[0]
+
+# Load the value-head model and tokenizer.
+tokenizer_name = script_args.model_name
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, use_fast=False)
+if script_args.model_name not in {
+    "gemma-2b-it",
+    "gemma-2-9b-it",
+    "gemma-2-27b-it",
+    "Qwen2-7B-Instruct",
+}:
+    tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+tokenizer.truncation_side = "left"
+tokenizer.model_max_length = script_args.max_length
+
+# Get the dataset
+train_path = script_args.train_set_path
+output_name = script_args.output_dir
+if script_args.load_data_from_local:
+    train_dataset = build_dataset_local(tokenizer, train_path)
+else:
+    train_dataset = build_dataset(tokenizer, train_path)
+print("Training set:", len(train_dataset))
+
+# Define the trainer
+training_args = TrainingArguments(
+    output_dir=output_name,
+    learning_rate=script_args.learning_rate,
+    per_device_train_batch_size=script_args.per_device_train_batch_size,
+    num_train_epochs=script_args.num_train_epochs,
+    weight_decay=script_args.weight_decay,
+    save_strategy="steps",
+    save_steps=script_args.save_every_steps,
+    gradient_accumulation_steps=script_args.gradient_accumulation_steps,
+    gradient_checkpointing=script_args.gradient_checkpointing,
+    remove_unused_columns=False,
+    label_names=[],
+    bf16=script_args.bf16,
+    logging_strategy="steps",
+    logging_steps=10,
+    optim=script_args.optim,
+    lr_scheduler_type=script_args.lr_scheduler_type,
+    warmup_ratio=0.03,
+    report_to="wandb",
+)
+
+model = AutoModelForSequenceClassification.from_pretrained(
+    script_args.model_name,
+    num_labels=1,
+    torch_dtype=torch.bfloat16,
+    attn_implementation="flash_attention_2",
+)
+model.config.use_cache = not script_args.gradient_checkpointing
+if script_args.model_name != "gemma-2b-it":
+    model.config.pad_token_id = tokenizer.pad_token_id
+    model.resize_token_embeddings(len(tokenizer))
+
+trainer = RewardTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    compute_metrics=compute_metrics,
+    data_collator=RewardDataCollatorWithPadding(
+        tokenizer=tokenizer, max_length=script_args.max_length
+    ),
+)
+trainer.train()
+trainer.save_model(output_name + "/last_checkpoint")
+tokenizer.save_pretrained(output_name + "/last_checkpoint")
---
+++ rm/utils.py
@@ -0,0 +1,136 @@
+import os
+from dataclasses import dataclass
+from typing import Any, Dict, List, Optional, Union
+
+import numpy as np
+import torch
+import torch.nn as nn
+from datasets import load_dataset, load_from_disk
+from transformers import AutoTokenizer, Trainer
+from transformers.utils import PaddingStrategy
+
+
+def build_dataset(tokenizer, train_path):
+
+    def tokenize(sample):
+        try:
+            sample["positive"] = tokenizer.apply_chat_template(
+                sample["chosen"], tokenize=False, add_generation_prompt=False
+            ).replace(tokenizer.bos_token, "")
+            sample["negative"] = tokenizer.apply_chat_template(
+                sample["rejected"], tokenize=False, add_generation_prompt=False
+            ).replace(tokenizer.bos_token, "")
+        except:
+            sample["positive"] = tokenizer.apply_chat_template(
+                sample["chosen"], tokenize=False, add_generation_prompt=False
+            )
+            sample["negative"] = tokenizer.apply_chat_template(
+                sample["rejected"], tokenize=False, add_generation_prompt=False
+            )
+        tokenized_pos = tokenizer(sample["positive"], truncation=True)
+        tokenized_neg = tokenizer(sample["negative"], truncation=True)
+        sample["input_ids_j"] = tokenized_pos["input_ids"]
+        sample["attention_mask_j"] = tokenized_pos["attention_mask"]
+        sample["input_ids_k"] = tokenized_neg["input_ids"]
+        sample["attention_mask_k"] = tokenized_neg["attention_mask"]
+        return sample
+
+    dataset = load_dataset(train_path, split="train").shuffle(seed=42)
+    dataset = dataset.map(tokenize, num_proc=os.cpu_count())
+    return dataset
+
+
+def build_dataset_local(tokenizer, train_path):
+
+    def tokenize(sample):
+        try:
+            sample["positive"] = tokenizer.apply_chat_template(
+                sample["chosen"], tokenize=False, add_generation_prompt=False
+            ).replace(tokenizer.bos_token, "")
+            sample["negative"] = tokenizer.apply_chat_template(
+                sample["rejected"], tokenize=False, add_generation_prompt=False
+            ).replace(tokenizer.bos_token, "")
+        except:
+            sample["positive"] = tokenizer.apply_chat_template(
+                sample["chosen"], tokenize=False, add_generation_prompt=False
+            )
+            sample["negative"] = tokenizer.apply_chat_template(
+                sample["rejected"], tokenize=False, add_generation_prompt=False
+            )
+        tokenized_pos = tokenizer(sample["positive"], truncation=True)
+        tokenized_neg = tokenizer(sample["negative"], truncation=True)
+        sample["input_ids_j"] = tokenized_pos["input_ids"]
+        sample["attention_mask_j"] = tokenized_pos["attention_mask"]
+        sample["input_ids_k"] = tokenized_neg["input_ids"]
+        sample["attention_mask_k"] = tokenized_neg["attention_mask"]
+        return sample
+
+    dataset = load_from_disk(train_path)["train"].shuffle(seed=42)
+    dataset = dataset.map(tokenize, num_proc=os.cpu_count())
+    return dataset
+
+
+@dataclass
+class RewardDataCollatorWithPadding:
+    tokenizer: AutoTokenizer
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+    return_tensors: str = "pt"
+
+    def __call__(self, features: List[Dict[str, Any]]) -> Dict[str, Any]:
+        merged_features = []
+        for feature in features:
+            merged_features.append(
+                {
+                    "input_ids": feature["input_ids_j"],
+                    "attention_mask": feature["attention_mask_j"],
+                }
+            )
+            merged_features.append(
+                {
+                    "input_ids": feature["input_ids_k"],
+                    "attention_mask": feature["attention_mask_k"],
+                }
+            )
+        batch = self.tokenizer.pad(
+            merged_features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors=self.return_tensors,
+        )
+        batch = {
+            "input_ids": batch["input_ids"],
+            "attention_mask": batch["attention_mask"],
+            "return_loss": True,
+        }
+        return batch
+
+
+# Define the trainer
+def compute_metrics(eval_pred):
+    result = {}
+    pos_predictions_scores = eval_pred.predictions[0]
+    neg_predictions_scores = eval_pred.predictions[1]
+    # We assume that the first sample is preferred by default in groundtruth
+    result["accuracy"] = np.sum(pos_predictions_scores > neg_predictions_scores) / len(
+        pos_predictions_scores
+    )
+    return result
+
+
+class RewardTrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        rewards = model(
+            input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"]
+        )[0]
+        bsz = rewards.size(0)
+        jidx = torch.arange(0, bsz, 2)
+        kidx = jidx + 1
+        rewards_j = rewards[jidx]
+        rewards_k = rewards[kidx]
+        loss = -nn.functional.logsigmoid(rewards_j - rewards_k).mean()
+        if return_outputs:
+            return loss, {"rewards_j": rewards_j, "rewards_k": rewards_k}
+        return loss

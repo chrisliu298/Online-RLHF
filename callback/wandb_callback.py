c28ea7566f25fbf5c71e4734f14e5f41558f23b6
chrisliu298 <cyliu298@proton.me>
2024-07-24 23:39:48 +0800

    Log kwargs during on_train_begin

--- callback/wandb_callback.py
+++ callback/wandb_callback.py
@@ -6,7 +6,12 @@ class CustomWandbCallback(WandbCallback):
         super().__init__()
         self.kwargs = kwargs
 
-    def setup(self, args, state, model, **kwargs):
-        super().setup(args, state, model, **kwargs)
+    # def setup(self, args, state, model, **kwargs):
+    #     super().setup(args, state, model, **kwargs)
+    #     # Log all kwargs
+    #     self._wandb.log(self.kwargs)
+
+    def on_train_begin(self, args, state, control, model=None, **kwargs):
+        super().on_train_begin(args, state, control, model, **kwargs)
         # Log all kwargs
         self._wandb.log(self.kwargs)

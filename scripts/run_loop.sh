3e64530b3036a8d6661e5d132d50c179a6167f15
chrisliu298 <cyliu298@proton.me>
2024-07-24 15:41:42 +0800

    Modify scripts

--- scripts/run_loop.sh
+++ scripts/run_loop.sh
@@ -1,19 +1,38 @@
-source ~/.bashrc
+# Initialize
+export http_proxy=""
+export https_proxy=$http_proxy
+export no_proxy="localhost,127.0.0.1,192.168.0.0/16,10.0.0.0/8"
+git config --global http.proxy $https_proxy
 
-# Initialize Conda environment
-eval "$(conda shell.bash hook)"
+# Clone the repository
+git clone https://github.com/chrisliu298/Online-RLHF.git
+cd Online-RLHF
 
+# Set up wandb
+export WANDB_ENTITY="skywork"
+export WANDB_PROJECT="qwen2-dpo-alignbench"
+export WANDB_API_KEY=""
+wandb login $WANDB_API_KEY
+wandb online
 
-# Base paths and settings
-initial_model="TODO: Set initial model path here" 
-#"meta-llama/Meta-Llama-3-8B-Instruct"
-base_path="TODO: Set base path here"
-#"/home/wx/Iterative-RLHF-dev/test"
-mkdir $base_path
-iteration_prefix="Test"
+# Install dependencies
+python -m pip uninstall -y flash-attn transformer_engine
+python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -U deepspeed bitsandbytes transformers trl peft accelerate vllm
+python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple flash-attn --no-build-isolation
 
+# Parameters
+initial_model="Qwen2-7B-Instruct"
+reward_model="Qwen2-7B-Instruct_preference_700K"
+base_path="/mnt/data/yuhaoliu/experiments/dpo/trial0"
+dataset_path="/mnt/data/yuhaoliu/datasets/dpo_datasets/alignbench_dpo_prompts"
+num_iters=5
+num_epochs=10
+lr=5e-7
+choose_type=max_min
 
-
+# Copy models
+cp -r /mnt/data/yuhaoliu/models/bt_models/${reward_model} /mnt/data/yuhaoliu/models/hf_models/${initial_model} .
+cp -r $dataset_path .
 
 # Function to run a set of operations for a model iteration
 run_iteration() {
@@ -23,34 +42,39 @@ run_iteration() {
     local json_output=$4
     local model_output=$5
 
-    conda activate vllm
+    echo "iteration: $iteration"
+    echo "model_path: $model_path"
+    echo "jsonl_input: $jsonl_input"
+    echo "json_output: $json_output"
+    echo "model_output: $model_output"
+
+    # Run generation
     bash generation/run_8gpu.sh $model_path
     sleep 60
-    python generation/gen_hf.py --ports 8000 8001 8002 8003 --eos_ids 128009 --tokenizer $initial_model --dataset_name_or_path $jsonl_input --output_dir $json_output --K 8 --temperature 1.0
+    python generation/gen_hf.py --ports 8000 8001 8002 8003 --eos_ids 151645 --tokenizer $initial_model --output_dir $json_output --K 8 --temperature 1.0 --dataset_name_or_path $jsonl_input
     pkill -f "python -m vllm.entrypoints.api_server"
-    accelerate launch annotate_data/get_rewards.py --dataset_name_or_path $json_output --output_dir $model_output
-    conda activate rlhflow
-    accelerate launch --config_file ./configs/zero2.yaml dpo_iteration/run_dpo.py --run_name $iteration --output_dir $iteration --model_name_or_path $model_path --ref_model $initial_model --learning_rate 5e-7 --max_steps 1200 --choose_type max_min --train_dir $model_output --eval_dir $model_output --loss_type sigmoid --lr_scheduler_type cosine
+
+    # Run reward labeling
+    accelerate launch annotate_data/get_rewards.py --reward_name_or_path $reward_model --dataset_name_or_path $json_output --output_dir $model_output
+
+    # Run DPO
+    accelerate launch --config_file ./configs/zero2.yaml dpo_iteration/run_dpo.py --run_name $iteration --output_dir $iteration --model_name_or_path $model_path --ref_model $initial_model --learning_rate $lr --choose_type $choose_type --train_data_path $model_output --eval_data_path $model_output --loss_type sigmoid --lr_scheduler_type cosine --num_train_epochs $num_epochs
 }
 
-
 # Main loop for iterations
-for i in {1..3}
+for i in $(seq 1 $num_iters)
 do
-    iteration_name="LLaMA3_iter${i}"
-    jsonl_input="RLHFlow/iterative-prompt-v1-iter${i}-20K"
-    json_output="${base_path}/${iteration_prefix}${i}_${iteration_name}.json"
-    model_output="${base_path}/${iteration_prefix}${i}_${iteration_name}_reward.json"
+    iteration_name="${initial_model}_iter${i}"
+    jsonl_input="${dataset_path}"
+    json_output="${base_path}/${iteration_name}.json"
+    model_output="${base_path}/${iteration_name}_reward.json"
     
-    # Determine the model path: first iteration uses the initial model, subsequent iterations use the previous iteration's model
     if [ $i -eq 1 ]; then
         model_path=$initial_model
     else
         previous_iteration=$((i-1))
-        model_path="LLaMA3_iter${previous_iteration}"
+        model_path="${initial_model}_iter${previous_iteration}"
     fi
 
     run_iteration $iteration_name $model_path $jsonl_input $json_output $model_output
 done
-
-

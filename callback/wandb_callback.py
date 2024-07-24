692fd46f4c0df070edcba9e28fedff8d37a01f83
chrisliu298 <cyliu298@proton.me>
2024-07-25 00:34:18 +0800

    Log during setup

--- callback/wandb_callback.py
+++ callback/wandb_callback.py
@@ -7,13 +7,13 @@ class CustomWandbCallback(WandbCallback):
         super().__init__()
         self.kwargs = kwargs
 
-    # def setup(self, args, state, model, **kwargs):
-    #     super().setup(args, state, model, **kwargs)
-    #     # Log all kwargs
-    #     self._wandb.log(self.kwargs)
-
-    def on_train_begin(self, args, state, control, model=None, **kwargs):
-        super().on_train_begin(args, state, control, model, **kwargs)
-        # Log all kwargs
+    def setup(self, args, state, model, **kwargs):
+        super().setup(args, state, model, **kwargs)
         if dist.get_rank() == 0:
             self._wandb.log(self.kwargs)
+
+    # def on_train_begin(self, args, state, control, model=None, **kwargs):
+    #     super().on_train_begin(args, state, control, model, **kwargs)
+    #     # Log all kwargs
+    #     if dist.get_rank() == 0:
+    #         self._wandb.log(self.kwargs)

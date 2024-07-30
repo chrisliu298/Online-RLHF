d26650b8a91aca58df2c42b52b028a2453e1cfd3
chrisliu298 <cyliu298@proton.me>
2024-07-30 13:42:09 +0800

    Change default num_generations

--- dpo/dpo_config.py
+++ dpo/dpo_config.py
@@ -8,5 +8,5 @@ class DPOConfigWithAdditionalArgs(DPOConfig):
     reward_model_name: str = None
     nll_loss_alpha: float = 0.0
     choose_type: str = None
-    num_generations: int = 2
+    num_generations: int = 8
     len_penalty: float = 0.0

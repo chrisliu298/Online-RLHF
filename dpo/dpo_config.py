ce162069502af43397c9dee2a9bdaed6b30b3b1e
chrisliu298 <cyliu298@proton.me>
2024-08-01 20:33:16 +0800

    Change default value

--- dpo/dpo_config.py
+++ dpo/dpo_config.py
@@ -6,7 +6,7 @@ from trl import DPOConfig
 @dataclass
 class DPOConfigWithAdditionalArgs(DPOConfig):
     choose_type: str = None
-    num_generations: int = 8
+    num_generations: int = None
     nll_loss_alpha: float = 0.0
     len_penalty: float = 0.0
     use_prev_iter_as_ref: str = "false"

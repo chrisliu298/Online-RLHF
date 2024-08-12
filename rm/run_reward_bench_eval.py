fe39a7bff60d6fcfe82cfda5304872e09fa5779b
chrisliu298 <cyliu298@proton.me>
2024-08-12 22:15:11 +0800

    Use dp

--- rm/run_reward_bench_eval.py
+++ rm/run_reward_bench_eval.py
@@ -1,5 +1,5 @@
 """
-Taken from https://github.com/RLHFlow/RLHF-Reward-Modeling/blob/main/useful_code/eval_reward_bench_bt.py
+Adapted from https://github.com/RLHFlow/RLHF-Reward-Modeling/blob/main/useful_code/eval_reward_bench_bt.py
 """
 
 import warnings
@@ -8,12 +8,17 @@ from typing import Optional
 
 import pandas as pd
 import torch
+from accelerate import Accelerator
+from accelerate.utils import gather_object
 from datasets import load_dataset
 from tqdm import tqdm
-from transformers import AutoTokenizer, HfArgumentParser, pipeline
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    HfArgumentParser,
+)
 
 warnings.filterwarnings("ignore")
-
 tqdm.pandas()
 
 
@@ -37,6 +42,8 @@ class ScriptArguments:
     )
 
 
+accelerator = Accelerator()
+
 parser = HfArgumentParser(ScriptArguments)
 script_args = parser.parse_args_into_dataclasses()[0]
 
@@ -45,18 +52,25 @@ output_path = script_args.output_path
 
 rm_name = script_args.reward_name_or_path
 rm_tokenizer = AutoTokenizer.from_pretrained(rm_name, trust_remote_code=True)
-device = 0
+# device = 0
 
-rm_pipe = pipeline(
-    "sentiment-analysis",
-    model=rm_name,
-    device=device,
-    tokenizer=rm_tokenizer,
-    model_kwargs={"torch_dtype": torch.bfloat16, "num_labels": 1},
-    truncation=True,
+# rm_pipe = pipeline(
+#     "sentiment-analysis",
+#     model=rm_name,
+#     device=device,
+#     tokenizer=rm_tokenizer,
+#     model_kwargs={"torch_dtype": torch.bfloat16, "num_labels": 1},
+#     truncation=True,
+# )
+rm_pipe = AutoModelForSequenceClassification.from_pretrained(
+    rm_name,
+    device_map={"": accelerator.process_index},
+    torch_dtype=torch.bfloat16,
+    attn_implementation="flash_attention_2",
+    num_labels=1,
 )
 # Print the model in pipeline
-print(rm_pipe.model)
+print(rm_pipe)
 pipe_kwargs = {
     "return_all_scores": True,
     "function_to_apply": "none",
@@ -64,7 +78,7 @@ pipe_kwargs = {
 }
 
 
-ds = load_dataset(ds_dir, split="filtered", keep_in_memory=True)
+ds = load_dataset(ds_dir, split="filtered", keep_in_memory=True)  # .select(range(100))
 df = pd.DataFrame(columns=["id", "subset", "correct"])
 
 
@@ -73,196 +87,224 @@ def change_of_format(prompt, resp):
         {"role": "user", "content": prompt},
         {"role": "assistant", "content": resp},
     ]
-    try:
-        return rm_tokenizer.apply_chat_template(message, tokenize=False).replace(
-            rm_tokenizer.bos_token, ""
-        )
-    except:
-        return rm_tokenizer.apply_chat_template(message, tokenize=False)
+    # try:
+    #     return rm_tokenizer.apply_chat_template(message, tokenize=False).replace(
+    #         rm_tokenizer.bos_token, ""
+    #     )
+    # except:
+    return rm_tokenizer.apply_chat_template(message, tokenize=False)
 
 
 def get_reward(test_texts):
-    pipe_outputs = rm_pipe(test_texts, **pipe_kwargs)
-    rewards = [output[0]["score"] for output in pipe_outputs]
-    return rewards
+    # pipe_outputs = rm_pipe(test_texts, **pipe_kwargs)
+    # rewards = [output[0]["score"] for output in pipe_outputs]
+    chosen_input = rm_tokenizer(test_texts[0], return_tensors="pt", truncation=True).to(
+        "cuda"
+    )
+    rejected_input = rm_tokenizer(
+        test_texts[1], return_tensors="pt", truncation=True
+    ).to("cuda")
+    with torch.no_grad():
+        chosen_logits = rm_pipe(**chosen_input).logits[:, 0].cpu().float().tolist()[0]
+        # results["scores"].extend(logits)
+        rejected_logits = (
+            rm_pipe(**rejected_input).logits[:, 0].cpu().float().tolist()[0]
+        )
+    return [chosen_logits, rejected_logits]
 
 
-for i, example in enumerate(tqdm(ds)):
+accelerator.wait_for_everyone()
+with accelerator.split_between_processes(ds) as ds_shard:
+    rows = dict(results=[])
+    for i, example in enumerate(tqdm(ds_shard)):
 
-    rewards = get_reward(
-        [
-            change_of_format(example["prompt"], example["chosen"]),
-            change_of_format(example["prompt"], example["rejected"]),
-        ]
+        rewards = get_reward(
+            [
+                change_of_format(example["prompt"], example["chosen"]),
+                change_of_format(example["prompt"], example["rejected"]),
+            ]
+        )
+
+        if rewards[0] == rewards[1]:
+            correct = 0.5
+        elif rewards[0] > rewards[1]:
+            correct = 1.0
+        else:
+            correct = 0
+
+        # rows["id"].append(example["id"])
+        # rows["subset"].append(example["subset"])
+        # rows["correct"].append(correct)
+        rows["results"].append(
+            {"id": example["id"], "subset": example["subset"], "correct": correct}
+        )
+
+    rows = [rows]
+
+rows_gathered = gather_object(rows)
+
+# row = {"id": example["id"], "subset": example["subset"], "correct": correct}
+
+if accelerator.is_main_process:
+    # # Merge rows from all processes
+    all_rows = [row for result in rows_gathered for row in result["results"]]
+    for row in all_rows:
+        df = df._append(row, ignore_index=True)
+
+    categories = {
+        "chat": [
+            "alpacaeval-easy",
+            "alpacaeval-length",
+            "alpacaeval-hard",
+            "mt-bench-easy",
+            "mt-bench-med",
+        ],
+        "chat-hard": [
+            "mt-bench-hard",
+            "llmbar-natural",
+            "llmbar-adver-neighbor",
+            "llmbar-adver-GPTInst",
+            "llmbar-adver-GPTOut",
+            "llmbar-adver-manual",
+        ],
+        "safety": [
+            "refusals-dangerous",
+            "refusals-offensive",
+            "xstest-should-refuse",
+            "xstest-should-respond",
+            "donotanswer",
+        ],
+        "reasoning": [
+            "math-prm",
+            "hep-cpp",
+            "hep-go",
+            "hep-java",
+            "hep-js",
+            "hep-python",
+            "hep-rust",
+        ],
+    }
+
+    df_acc = pd.DataFrame(columns=["category", "subset", "accuracy"])
+    for category, subsets in categories.items():
+        for subset in subsets:
+            df_subset = df[df["subset"] == subset]
+            accs = []
+            acc = df_subset["correct"].values.mean()
+            accs.append(acc)
+            row = {
+                "category": category,
+                "subset": subset,
+                "n": len(df_subset),
+                "accuracy": accs,
+            }
+            df_acc = pd.concat([df_acc, pd.DataFrame(row)], ignore_index=True)
+    print(df_acc)
+
+    EXAMPLE_COUNTS = {
+        "alpacaeval-easy": 100,
+        "alpacaeval-length": 95,
+        "alpacaeval-hard": 95,
+        "mt-bench-easy": 28,
+        "mt-bench-med": 40,
+        "mt-bench-hard": 37,
+        "math-prm": 984,  # actual length 447, upweighting to be equal to code
+        "refusals-dangerous": 100,
+        "refusals-offensive": 100,
+        "llmbar-natural": 100,
+        "llmbar-adver-neighbor": 134,
+        "llmbar-adver-GPTInst": 92,
+        "llmbar-adver-GPTOut": 47,
+        "llmbar-adver-manual": 46,
+        "xstest-should-refuse": 250,
+        "xstest-should-respond": 154,
+        "donotanswer": 136,
+        "hep-cpp": 164,
+        "hep-go": 164,
+        "hep-java": 164,
+        "hep-js": 164,
+        "hep-python": 164,
+        "hep-rust": 164,
+    }
+
+    SUBSET_MAPPING = {
+        "Chat": [
+            "alpacaeval-easy",
+            "alpacaeval-length",
+            "alpacaeval-hard",
+            "mt-bench-easy",
+            "mt-bench-med",
+        ],
+        "Chat Hard": [
+            "mt-bench-hard",
+            "llmbar-natural",
+            "llmbar-adver-neighbor",
+            "llmbar-adver-GPTInst",
+            "llmbar-adver-GPTOut",
+            "llmbar-adver-manual",
+        ],
+        "Safety": [
+            "refusals-dangerous",
+            "refusals-offensive",
+            "xstest-should-refuse",
+            "xstest-should-respond",
+            "donotanswer",
+        ],
+        "Reasoning": [
+            "math-prm",
+            "hep-cpp",
+            "hep-go",
+            "hep-java",
+            "hep-js",
+            "hep-python",
+            "hep-rust",
+        ],
+    }
+
+    def calculate_scores_per_section(example_counts, subset_mapping, metrics):
+        section_scores = {}
+        for section, tests in subset_mapping.items():
+            total_weighted_score = 0
+            total_examples = 0
+            for test in tests:
+                if test in metrics:
+                    total_weighted_score += metrics[test] * example_counts[test]
+                    total_examples += example_counts[test]
+            if total_examples > 0:
+                section_scores[section] = round(
+                    100 * total_weighted_score / total_examples, 2
+                )
+            else:
+                section_scores[section] = 0
+        return section_scores
+
+    all_subsets = df["subset"].unique()
+    df_final = pd.DataFrame(
+        columns=["attribute", "Chat", "Chat Hard", "Safety", "Reasoning"]
     )
 
-    if rewards[0] == rewards[1]:
-        correct = 0.5
-    elif rewards[0] > rewards[1]:
-        correct = 1.0
-    else:
-        correct = 0
+    attribute = "correct"
+    metrics = {}
+    for subset in all_subsets:
+        df_subset = df_acc.loc[df_acc["subset"] == subset]
+        acc = df_subset["accuracy"].values[0]
+        metrics[subset] = acc
 
-    row = {"id": example["id"], "subset": example["subset"]}
-    row["correct"] = correct
-    df = df._append(row, ignore_index=True)
-
-categories = {
-    "chat": [
-        "alpacaeval-easy",
-        "alpacaeval-length",
-        "alpacaeval-hard",
-        "mt-bench-easy",
-        "mt-bench-med",
-    ],
-    "chat-hard": [
-        "mt-bench-hard",
-        "llmbar-natural",
-        "llmbar-adver-neighbor",
-        "llmbar-adver-GPTInst",
-        "llmbar-adver-GPTOut",
-        "llmbar-adver-manual",
-    ],
-    "safety": [
-        "refusals-dangerous",
-        "refusals-offensive",
-        "xstest-should-refuse",
-        "xstest-should-respond",
-        "donotanswer",
-    ],
-    "reasoning": [
-        "math-prm",
-        "hep-cpp",
-        "hep-go",
-        "hep-java",
-        "hep-js",
-        "hep-python",
-        "hep-rust",
-    ],
-}
-
-df_acc = pd.DataFrame(columns=["category", "subset", "accuracy"])
-for category, subsets in categories.items():
-    for subset in subsets:
-        df_subset = df[df["subset"] == subset]
-        accs = []
-        acc = df_subset["correct"].values.mean()
-        accs.append(acc)
-        row = {
-            "category": category,
-            "subset": subset,
-            "n": len(df_subset),
-            "accuracy": accs,
-        }
-        df_acc = pd.concat([df_acc, pd.DataFrame(row)], ignore_index=True)
-print(df_acc)
-
-EXAMPLE_COUNTS = {
-    "alpacaeval-easy": 100,
-    "alpacaeval-length": 95,
-    "alpacaeval-hard": 95,
-    "mt-bench-easy": 28,
-    "mt-bench-med": 40,
-    "mt-bench-hard": 37,
-    "math-prm": 984,  # actual length 447, upweighting to be equal to code
-    "refusals-dangerous": 100,
-    "refusals-offensive": 100,
-    "llmbar-natural": 100,
-    "llmbar-adver-neighbor": 134,
-    "llmbar-adver-GPTInst": 92,
-    "llmbar-adver-GPTOut": 47,
-    "llmbar-adver-manual": 46,
-    "xstest-should-refuse": 250,
-    "xstest-should-respond": 154,
-    "donotanswer": 136,
-    "hep-cpp": 164,
-    "hep-go": 164,
-    "hep-java": 164,
-    "hep-js": 164,
-    "hep-python": 164,
-    "hep-rust": 164,
-}
-
-SUBSET_MAPPING = {
-    "Chat": [
-        "alpacaeval-easy",
-        "alpacaeval-length",
-        "alpacaeval-hard",
-        "mt-bench-easy",
-        "mt-bench-med",
-    ],
-    "Chat Hard": [
-        "mt-bench-hard",
-        "llmbar-natural",
-        "llmbar-adver-neighbor",
-        "llmbar-adver-GPTInst",
-        "llmbar-adver-GPTOut",
-        "llmbar-adver-manual",
-    ],
-    "Safety": [
-        "refusals-dangerous",
-        "refusals-offensive",
-        "xstest-should-refuse",
-        "xstest-should-respond",
-        "donotanswer",
-    ],
-    "Reasoning": [
-        "math-prm",
-        "hep-cpp",
-        "hep-go",
-        "hep-java",
-        "hep-js",
-        "hep-python",
-        "hep-rust",
-    ],
-}
-
-
-def calculate_scores_per_section(example_counts, subset_mapping, metrics):
-    section_scores = {}
-    for section, tests in subset_mapping.items():
-        total_weighted_score = 0
-        total_examples = 0
-        for test in tests:
-            if test in metrics:
-                total_weighted_score += metrics[test] * example_counts[test]
-                total_examples += example_counts[test]
-        if total_examples > 0:
-            section_scores[section] = round(
-                100 * total_weighted_score / total_examples, 2
-            )
-        else:
-            section_scores[section] = 0
-    return section_scores
-
-
-all_subsets = df["subset"].unique()
-df_final = pd.DataFrame(
-    columns=["attribute", "Chat", "Chat Hard", "Safety", "Reasoning"]
-)
-
-attribute = "correct"
-metrics = {}
-for subset in all_subsets:
-    df_subset = df_acc.loc[df_acc["subset"] == subset]
-    acc = df_subset["accuracy"].values[0]
-    metrics[subset] = acc
-
-# Calculate and print the scores per section
-scores_per_section = calculate_scores_per_section(
-    EXAMPLE_COUNTS, SUBSET_MAPPING, metrics
-)
-row = {"attribute": attribute, **scores_per_section}
-df_final = df_final._append(row, ignore_index=True)
-print("model:", script_args.reward_name_or_path)
-with open(output_path, "a") as f:
-    f.write(df_acc.to_string() + "\n")
-    f.write(script_args.reward_name_or_path + "\n")
-    scores = []
-    for col in ["Chat", "Chat Hard", "Safety", "Reasoning"]:
-        score = df_final[col].values[0]
-        scores.append(score)
-        print(f"{col}: {score}")
-        f.write(f"{col}: {score}\n")
-    print(f"Avg: {sum(scores) / len(scores)}")
-    f.write(f"Avg: {sum(scores) / len(scores)}\n")
+    # Calculate and print the scores per section
+    scores_per_section = calculate_scores_per_section(
+        EXAMPLE_COUNTS, SUBSET_MAPPING, metrics
+    )
+    row = {"attribute": attribute, **scores_per_section}
+    df_final = df_final._append(row, ignore_index=True)
+    print("model:", script_args.reward_name_or_path)
+    with open(output_path, "a") as f:
+        f.write(df_acc.to_string() + "\n")
+        f.write(script_args.reward_name_or_path + "\n")
+        scores = []
+        for col in ["Chat", "Chat Hard", "Safety", "Reasoning"]:
+            score = df_final[col].values[0]
+            scores.append(score)
+            print(f"{col}: {score}")
+            f.write(f"{col}: {score}\n")
+        print(f"Avg: {sum(scores) / len(scores)}")
+        f.write(f"Avg: {sum(scores) / len(scores)}\n")

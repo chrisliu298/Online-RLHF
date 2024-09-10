303fb2340d1bd5ac5fbd6941643a5094f3564bfb
chrisliu298 <cyliu298@proton.me>
2024-09-10 19:31:40 +0800

    Update utils.py

--- rm/utils.py
+++ rm/utils.py
@@ -330,10 +330,15 @@ class RewardTrainer(Trainer):
             )
         elif self.loss_type == "focal_penalty":
             margin_prob = torch.sigmoid(rewards_j - rewards_k)
-            ranking_loss = -(
-                (1 - 2 * max(torch.zeros_like(margin_prob), margin_prob - 0.5)).mean()
-                ** self.gamma
-            ) * torch.log(margin_prob)
+            ranking_loss = (
+                -(
+                    (
+                        1 - 2 * max(torch.zeros_like(margin_prob), margin_prob - 0.5)
+                    ).mean()
+                    ** self.gamma
+                )
+                * torch.log(margin_prob).mean()
+            )
             penalty_j = -(torch.log(rewards_j + 5) + torch.log(5 - rewards_j)).mean()
             penalty_k = -(torch.log(rewards_k + 5) + torch.log(5 - rewards_k)).mean()
             loss = ranking_loss + self.lambd * penalty_j + self.lambd * penalty_k

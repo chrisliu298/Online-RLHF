384cd9c657f0403a54b70891e9333ac8700f5c03
chrisliu298 <cyliu298@proton.me>
2024-09-10 08:53:31 +0800

    Update chat_templates.py

--- sft/chat_templates.py
+++ sft/chat_templates.py
@@ -2,13 +2,13 @@ from transformers import AutoTokenizer
 
 chat_templates = {
     "Meta-Llama-3-8B": AutoTokenizer.from_pretrained(
-        "/mnt/data/yuhaoliu/models/hf_models/Meta-Llama-3-8B"
+        "/mnt/data/yuhaoliu/models/hf_models/Meta-Llama-3-8B-Instruct"
     ).chat_template,
     "Meta-Llama-3.1-8B": AutoTokenizer.from_pretrained(
-        "/mnt/data/yuhaoliu/models/hf_models/Meta-Llama-3.1-8B"
+        "/mnt/data/yuhaoliu/models/hf_models/Meta-Llama-3.1-8B-Instruct"
     ).chat_template,
     "gemma-2-9b": AutoTokenizer.from_pretrained(
-        "/mnt/data/yuhaoliu/models/hf_models/gemma-2-9b"
+        "/mnt/data/yuhaoliu/models/hf_models/gemma-2-9b-it"
     ).chat_template,
     "Meta-Llama-3-8B-Instruct": AutoTokenizer.from_pretrained(
         "/mnt/data/yuhaoliu/models/hf_models/Meta-Llama-3-8B-Instruct"

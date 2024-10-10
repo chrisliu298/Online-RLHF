7754f9bbfc01527c9edf09ea87f985e84b0fa9ab
chrisliu298 <cyliu298@proton.me>
2024-10-10 13:30:05 -0700

    Update run_reward_modeling.py

--- rm/run_reward_modeling.py
+++ rm/run_reward_modeling.py
@@ -1,6 +1,5 @@
 import json
 import os
-import random
 from dataclasses import dataclass, field
 from typing import Optional
 
@@ -17,10 +16,6 @@ from utils import (
     build_dataset_local,
     compute_metrics,
 )
-
-# torch.cuda.set_per_process_memory_fraction(0.99, torch.cuda.current_device())
-seed = random_seed = random.randint(0, 2**32 - 1)
-data_seed = random.randint(0, 2**32 - 1)
 
 
 @dataclass
@@ -128,6 +123,7 @@ class ScriptArguments:
     temp: Optional[float] = field(
         default=1.0, metadata={"help": "The temperature for the reward model"}
     )
+    seed: Optional[int] = field(default=42, metadata={"help": "The seed"})
 
 
 parser = HfArgumentParser(ScriptArguments)
@@ -193,8 +189,7 @@ training_args = TrainingArguments(
     neftune_noise_alpha=script_args.neftune_noise_alpha,
     # eval_strategy="steps",
     # eval_steps=script_args.save_steps,
-    seed=seed,
-    data_seed=data_seed,
+    seed=script_args.seed,
 )
 model = AutoModelForSequenceClassification.from_pretrained(
     script_args.model_name,
@@ -252,7 +247,4 @@ tokenizer.save_pretrained(script_args.output_dir)
 with open(os.path.join(script_args.output_dir, "script_args.json"), "w") as f:
     # Convert script_args to a dictionary
     script_args_dict = script_args.__dict__
-    # Add the random seeds to the dictionary
-    script_args_dict["seed"] = seed
-    script_args_dict["data_seed"] = data_seed
     json.dump(script_args_dict, f)

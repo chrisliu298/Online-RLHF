b35f7a74cad350b281f70f4d85a697e023a54bde
chrisliu298 <cyliu298@proton.me>
2024-10-14 16:54:56 -0700

    Update utils.py

--- rm/utils.py
+++ rm/utils.py
@@ -2,12 +2,15 @@ import os
 from dataclasses import dataclass
 from typing import Any, Dict, List, Optional, Union
 
+import datasets
 import numpy as np
 import torch
 import torch.nn as nn
 from datasets import load_dataset, load_from_disk
+from torch.utils.data import DataLoader
 from transformers import AutoTokenizer, Trainer
-from transformers.utils import PaddingStrategy
+from transformers.trainer_utils import seed_worker
+from transformers.utils import PaddingStrategy, is_datasets_available
 
 special_tokens = {
     "gemma-2-27b-it": "<unused0>",
@@ -422,3 +425,44 @@ class RewardTrainer(Trainer):
             self.log({"rewards/rejected": rewards_k.mean().item()})
 
         return loss
+
+    def get_train_dataloader(self) -> DataLoader:
+        """
+        Returns the training [`~torch.utils.data.DataLoader`].
+
+        Will use no sampler if `train_dataset` does not implement `__len__`, a random sampler (adapted to distributed
+        training if necessary) otherwise.
+
+        Subclass and override this method if you want to inject some custom behavior.
+        """
+        if self.train_dataset is None:
+            raise ValueError("Trainer: training requires a train_dataset.")
+
+        train_dataset = self.train_dataset
+        data_collator = self.data_collator
+        if is_datasets_available() and isinstance(train_dataset, datasets.Dataset):
+            train_dataset = self._remove_unused_columns(
+                train_dataset, description="training"
+            )
+        else:
+            data_collator = self._get_collator_with_removed_columns(
+                data_collator, description="training"
+            )
+
+        dataloader_params = {
+            "batch_size": self._train_batch_size,
+            "collate_fn": data_collator,
+            "num_workers": self.args.dataloader_num_workers,
+            "pin_memory": self.args.dataloader_pin_memory,
+            "persistent_workers": self.args.dataloader_persistent_workers,
+        }
+
+        if not isinstance(train_dataset, torch.utils.data.IterableDataset):
+            dataloader_params["sampler"] = self._get_train_sampler()
+            dataloader_params["drop_last"] = self.args.dataloader_drop_last
+            dataloader_params["worker_init_fn"] = seed_worker
+            dataloader_params["prefetch_factor"] = self.args.dataloader_prefetch_factor
+
+        return self.accelerator.prepare(
+            DataLoader(train_dataset, shuffle=False, **dataloader_params)
+        )
